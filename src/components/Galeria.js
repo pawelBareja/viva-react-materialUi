281d3e6f4abb5046bbaeb5792bdb3cf0a5fa84c1
Pawel Bareja <bareja.pawel@gmail.com>
2019-12-27 19:41:25 +0100

    galery component

--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -1,42 +1,103 @@
+import React, { useState, useCallback } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import img1 from "../assets/images/gallery/1.jpg"
+import img2 from "../assets/images/gallery/2.jpg"
+import img3 from "../assets/images/gallery/3.jpg"
+import img4 from "../assets/images/gallery/4.jpg"
+import img5 from "../assets/images/gallery/5.jpg"
+import img6 from "../assets/images/gallery/6.jpg"
+import img7 from "../assets/images/gallery/7.jpg"
+import img8 from "../assets/images/gallery/8.jpg"
+import img9 from "../assets/images/gallery/9.jpg"
 
-import React from "react"
-import Gallery from 'react-photo-gallery';
-import img1 from "../assets/images/gallery/1.jpg";
-import img2 from "../assets/images/gallery/2.jpg";
-import img3 from "../assets/images/gallery/3.jpg";
-import img4 from "../assets/images/gallery/4.jpg";
-import img5 from "../assets/images/gallery/5.jpg";
-import img6 from "../assets/images/gallery/6.jpg";
-import img7 from "../assets/images/gallery/7.jpg";
 
-
-
-export default class Galeria extends React.Component {
-    render() {
-        return (
-            <Gallery photos={PHOTO_SET} />
-        );
+const photos = [
+    {
+        src: img1,
+        width: 4,
+        height: 3
+    },
+    {
+        src: img2,
+        width: 2,
+        height: 1
+    },
+    {
+        src: img3,
+        width: 4,
+        height: 3
+    },
+    {
+        src: img4,
+        width: 4,
+        height: 3
+    },
+    {
+        src: img5,
+        width: 4,
+        height: 3
+    },
+    {
+        src: img6,
+        width: 4,
+        height: 3
+    },
+    {
+        src: img7,
+        width: 4,
+        height: 3
+    },
+    {
+        src: img8,
+        width: 2,
+        height: 1
+    },
+    {
+        src: img9,
+        width: 4,
+        height: 3
     }
-}
-const PHOTO_SET = [
-    {
-        src: img1,
-        width: 4,
-        height: 3
-    },
-    {
-        src: img2,
-        width: 4,
-        height: 3
-    },
-    {
-        src: img1,
-        width: 4,
-        height: 3
-    },
-    {
-        src: img2,
-        width: 4,
-        height: 3
-    },
 ];
+
+
+const galleryStyle = {
+    margin: "50px 0",
+}
+
+function Galeria() {
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
+
+    return (
+        <div style={galleryStyle}>
+            <Gallery photos={photos} onClick={openLightbox} />
+            <ModalGateway>
+                {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                            currentIndex={currentImage}
+                            views={photos.map(x => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title
+                            }))}
+                        />
+                    </Modal>
+                ) : null}
+            </ModalGateway>
+        </div>
+    );
+}
+
+export default Galeria;
